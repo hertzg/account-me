f2919d2c6e71ac2c51ca0bdf5824e72ddfc10faf
hoffman.philip.a@gmail.com <hoffman.philip.a@gmail.com@9ea2f819-3547-255c-3756-4898320dcaa7>
2011-08-09 18:45:21 +0000

    Initial database design

--- trunk/mysite/account_me/models.py
+++ trunk/mysite/account_me/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Transaction(models.Model):
+   transDate          = models.DateField()
+   transAmmount       = models.DecimalField(decimal_places=3)
+   transDescription   = models.CharField(max_length=100)
+   accountId          = models.ForeignKey(Account)
+   vendorId           = models.ForeignKey(Vendor)
+   categoryID         = models.ForeignKey(Category)
+
+class Account(models.Model):
+   acctName    = models.CharField(max_length=50)
+   acctNumber  = models.IntegerField(max_length=25)
+   acctType    = models.CharField(max_length=50)   #should be a custom field with options checking savings creditcard etc
+   bankId      = models.ForeignKey(Bank)
+
+class Bank:
+   bankName       = models.CharField(max_length=50)
+   bankRoutingNum = models.IntegerField(max_length=10)
+
+class Vendor(models.Model):
+   vendorName           = models.CharField(max_length=50)
+   vendorAddress        = models.CharField(max_length=50)
+   vendorCity           = models.CharField(max_length=60)
+   vendorState_province = models.CharField(max_length=30)
+   vendorCountry        = models.CharField(max_length=50)
+   vendorWebsite        = models.URLField()
+
+class Category(models.Model):
+  catName        = models.CharField(max_length=50)
+  catDescription = models.CharField(max_length=100)
+  catSubCat      = models.ForeignKey(Category)
